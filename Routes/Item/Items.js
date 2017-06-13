42f83b8edc65fe0ee72825e638ae840a397d6e41
franswah <jfrancis2795@gmail.com>
2017-06-13 00:21:19 -0700

    Added PUT and DELETE Item

--- Routes/Item/Items.js
+++ Routes/Item/Items.js
@@ -28,7 +28,7 @@ router.get('/', function (req, res) {
       });
 });
 
-router.get('/:id', function (req, res) {
+router.get('/:itemId', function (req, res) {
    var vld = req.validator;
    var dist = req.query.radius;
    var title = req.query.title;
@@ -36,7 +36,7 @@ router.get('/:id', function (req, res) {
    req.cnn.query('select i.id, title, price, ownerId, zip, latitude,' +
    ' longitude, description, categoryId,' +
     ' postTime, email, imageUrl from Item i join User u on ownerId = u.id' +
-    ' where i.id = ?', [req.params.id],
+    ' where i.id = ?', [req.params.itemId],
       function (err, itemArr) {
          if (vld.check(itemArr.length, Tags.notFound)) {
             var item = itemArr[0];
@@ -52,5 +52,52 @@ router.get('/:id', function (req, res) {
       });
 });
 
+router.put('/:itemId', function (req, res) {
+   var vld = req.validator;
+   var body = req.body;
+   var cnn = req.cnn;
+   var itemId = req.params.itemId;
+
+   async.waterfall([
+   function (cb) {
+      if (vld.hasOnlyFields(body, ["title", "description", "price"], cb)) {
+         cnn.chkQry('select * from Item where id = ?', [itemId], cb);
+      }
+   },
+   function (result, fields, cb) {
+      if (vld.check(result.length, Tags.notFound, null, cb) &&
+       vld.checkPrsOK(result[0].ownerId, cb))
+         cnn.chkQry("update Item set ? where id = ?", 
+          [body, itemId], cb);
+   },
+   function (result, fields, cb) {
+      res.status(200).end();
+      cb();
+   }],
+   function () {
+      cnn.release();
+   });
+});
+
+router.delete('/:itemId', function (req, res) {
+   var vld = req.validator;
+   var itemId = req.params.itemId;
+   var cnn = req.cnn;
+
+   async.waterfall([
+   function (cb) {
+      cnn.chkQry('select * from Item where id = ?', [itemId], cb);
+   },
+   function (result, fields, cb) {
+      if (vld.check(result.length, Tags.notFound, null, cb) &&
+       vld.checkPrsOK(result[0].ownerId, cb))
+         cnn.chkQry('delete from Item where id = ?', [itemId], cb);
+   }],
+   function (err) {
+      if (!err)
+         res.status(200).end();
+      cnn.release();
+   });
+});
 
 module.exports = router;
