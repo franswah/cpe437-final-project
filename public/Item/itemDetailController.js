cdf2ac8e5dc30aa372c2db2b4e8115ec8ec119f4
jkehl <jkehl@calpoly.edu>
2017-06-13 17:54:20 -0700

    Added itemListings (directive) to itemOverview page, fixed overview, added itemDetail page.

--- public/Item/itemDetailController.js
+++ public/Item/itemDetailController.js
@@ -1,22 +1,9 @@
-app.controller('cnvDetailController',
- ['$scope', '$state', '$http', 'cnv', 'msgs',
- function($scope, $state, $http, cnv, msgs) {
+app.controller('itemDetailController',
+ ['$scope', '$state', '$http', 'item',
+ function($scope, $state, $http, item) {
+   $scope.item = item;
 
-   $scope.cnv = cnv;
-   $scope.msgs = msgs;
-
-   $scope.send = function(newContent) {   
-      $http.post('Cnvs/' + cnv.id + '/Msgs', {content: newContent})
-      .then(function() {
-         return $http.get('Cnvs/' + cnv.id + '/Msgs');
-      })
-      .then(function(rsp) {
-         $scope.msgs = rsp.data;
-         $scope.content = '';
-      })
-      .catch(function() {
-         nDlg.show($scope, "Message failed to send.",
-          "Error");
-      });
+   $scope.editItem = function() {
+      ;//TBD
    }
 }]);
